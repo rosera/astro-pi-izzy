a74e2e892194d9d656b65770bc739a32a56277f5
rosie <askrichardrose@gmail.com>
2020-02-11 08:05:07 +0000

    Add files via upload
    Initial camera code

---
+++ cam_capture_IZ.py
@@ -0,0 +1,64 @@
+
+
+
+from time import sleep
+from fractions import Fraction
+
+import ephem
+from picamera import PiCamera
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+name = "ISS (ZARYA)"
+l1 = "1 25544U 98067A   18030.93057008  .00011045  00000-0  17452-3 0  9997"
+l2 = "2 25544  51.6392 342.9681 0002977  45.8872  32.8379 15.54020911 97174"
+iss = ephem.readtle(name, l1, l2)
+
+# Force sensor mode 3 (the long exposure mode), set
+# the framerate to 1/6fps, the shutter speed to 6s,
+# and ISO to 800 (for maximum gain)
+cam = PiCamera(
+    resolution=(1280, 720),
+    framerate=Fraction(1, 6),
+    sensor_mode=3)
+cam.shutter_speed = 6000000
+cam.iso = 800
+# Give the camera a good long time to set gains and
+# measure AWB (you may wish to use fixed AWB instead)
+
+
+iss.compute()
+
+def get_latlon():
+    iss.compute() # Get the lat/long values from ephem
+
+    long_value = [float(i) for i in str(iss.sublong).split(":")]
+
+    if long_value[0] < 0:
+
+        long_value[0] = abs(long_value[0])
+        cam.exif_tags['GPS.GPSLongitudeRef'] = "W"
+    else:
+        cam.exif_tags['GPS.GPSLongitudeRef'] = "E"
+    cam.exif_tags['GPS.GPSLongitude'] = '%d/1,%d/1,%d/10' % (long_value[0], long_value[1], long_value[2]*10)
+
+    lat_value = [float(i) for i in str(iss.sublat).split(":")]
+
+    if lat_value[0] < 0:
+
+        lat_value[0] = abs(lat_value[0])
+        cam.exif_tags['GPS.GPSLatitudeRef'] = "S"
+    else:
+        cam.exif_tags['GPS.GPSLatitudeRef'] = "N"
+
+    cam.exif_tags['GPS.GPSLatitude'] = '%d/1,%d/1,%d/10' % (lat_value[0], lat_value[1], lat_value[2]*10)
+    print(str(lat_value), str(long_value))
+
+get_latlon()
+
+sleep(30)
+cam.exposure_mode = 'off'
+# Finally, capture an image with a 6s exposure. Due
+# to mode switching on the still port, this will take
+# longer than 6 seconds
+cam.capture(dir_path+"/IZ_dark.jpg")
