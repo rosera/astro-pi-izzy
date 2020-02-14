05b2c531a545ef54dcf2eb8a367c27df90f8e568
rosera <technologynorth@gmail.com>
2020-02-14 17:33:27 +0000

    Tidy code

---
+++ old_code/cam_capture_IZ_v2.py
@@ -0,0 +1,42 @@
+import ephem
+from picamera import PiCamera
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+name = "ISS (ZARYA)"
+l1 = "1 25544U 98067A   18030.93057008  .00011045  00000-0  17452-3 0  9997"
+l2 = "2 25544  51.6392 342.9681 0002977  45.8872  32.8379 15.54020911 97174"
+iss = ephem.readtle(name, l1, l2)
+
+cam = PiCamera()
+cam.resolution = (1296,972) # Valid resolution for V1 camera
+iss.compute()
+
+def get_latlon():
+    iss.compute() # Get the lat/long values from ephem
+
+    long_value = [float(i) for i in str(iss.sublong).split(":")]
+
+    if long_value[0] < 0:
+
+        long_value[0] = abs(long_value[0])
+        cam.exif_tags['GPS.GPSLongitudeRef'] = "W"
+    else:
+        cam.exif_tags['GPS.GPSLongitudeRef'] = "E"
+    cam.exif_tags['GPS.GPSLongitude'] = '%d/1,%d/1,%d/10' % (long_value[0], long_value[1], long_value[2]*10)
+
+    lat_value = [float(i) for i in str(iss.sublat).split(":")]
+
+    if lat_value[0] < 0:
+
+        lat_value[0] = abs(lat_value[0])
+        cam.exif_tags['GPS.GPSLatitudeRef'] = "S"
+    else:
+        cam.exif_tags['GPS.GPSLatitudeRef'] = "N"
+
+    cam.exif_tags['GPS.GPSLatitude'] = '%d/1,%d/1,%d/10' % (lat_value[0], lat_value[1], lat_value[2]*10)
+    print(str(lat_value), str(long_value))
+
+get_latlon()
+
+cam.capture(dir_path+"/gps1.jpg")
